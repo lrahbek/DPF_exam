00168aeae7dc5d45393c72faef555b5f354aafe1
lrahbek <laura.rahbek@gmail.com>
2025-05-17 15:12:29 +0200

    functions for loading data + prep

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # DPF_exam
 
 to activate env in both terminal and nb write ```source activate.sh´´´
+
+scrpts should be run from top folder (DPF_exam)
---
+++ src/download_data.py
@@ -0,0 +1,33 @@
+import os 
+import pathlib
+import gdown
+import pyarrow
+import pandas as pd
+import numpy as np
+import itertools
+from pooch import DOIDownloader
+
+
+
+def load_chorus(data_path = "data/chorus"):
+    """ 
+    Download Chorus data: Hall, Galen, Joshua Basseches, Rebecca Bromley-Trujillo, and Trevor Culhane. 2023. 
+    "CHORUS: A new dataset of state interest group policy positions in the United States." State Politics & 
+    Policy Quarterly. Forthcoming 2023.
+    """
+    path = pathlib.Path(data_path)
+    path.mkdir(exist_ok=True)
+    data_url = "https://drive.google.com/drive/folders/1JLxwurbx0ys4DUDB2o-WCtWWsjisVi8L?usp=sharing"
+    gdown.download_folder(data_url, output = data_path)
+    return print(f"Data downloaded from google drive to {data_path}")
+
+
+def load_shormccarty(data_path = "data/shormccarty/state_ideology.tab"):
+    """
+    Download data: Shor, Boris, 2020, "Aggregate State Legislator Shor-McCarty Ideology Data, 
+    July 2020 update", https://doi.org/10.7910/DVN/AP54NE, Harvard Dataverse. 
+    """
+    downloader = pooch.DOIDownloader()
+    url = "doi:10.7910/DVN/AP54NE/shor mccarty 1993-2018 state aggregate data July 2020 release.tab"
+    downloader(url=url, output_file=data_path, pooch=None)
+    return print(f"file saved correctly at {data_path}: {os.path.exists(data_path)}")
